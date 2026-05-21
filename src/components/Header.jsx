374ee1cba999678195e972ebf66dfd958dc1396d
Álvaro Benitez Mayorga <alvarobenitezmayorga@gmail.com>
2026-05-21 20:30:58 +0200

    Home Page y Components Header, Navbar, y Footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+function Header() {
+  return (
+    <header>
+      <div>Nombre de la marca</div>
+      <Navbar />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home(){
+    return <main>
+        <h1>Catálogo mobiliario</h1>
+    </main>;
+}
+
+export default Home;
